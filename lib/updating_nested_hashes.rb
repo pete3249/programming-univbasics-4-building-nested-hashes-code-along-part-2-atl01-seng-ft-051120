b773bfc9023c23f2214ad9ffc7277173ba363b25
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-04-21 19:12:53 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -59,8 +59,7 @@ def add_characters
    #   - The second hash should have the :name key set to "Apothecary" 
    # Code your solution here:
 
-epic_tragedy[:montague][:hero][:status] = "dead"
-epic_tragedy[:capulet][:heroine][:status] = "dead"
+
 
    # After your code, the following line will return the altered hash
    epic_tragedy
